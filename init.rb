694389d295dc84075ffc6ba4bf501fa04549f2fe
Roberto Piccini <roberto.piccini@gmail.com>
2018-04-19 12:26:59 +0200

    redmine version 3.x

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require 'redmine_html_mail_handler_notifier/html_mail_handler_notifier_logger'
 
 Rails.configuration.to_prepare do
   unless MailHandler.included_modules.include? RedmineHtmlMailHandlerNotifier::MailHandlerPatch
-    MailHandler.send(:include, RedmineHtmlMailHandlerNotifier::MailHandlerPatch)
+    MailHandler.prepend(RedmineHtmlMailHandlerNotifier::MailHandlerPatch)
   end
 end
 
@@ -11,10 +11,9 @@ Redmine::Plugin.register :redmine_html_mail_handler_notifier do
   name 'Redmine Html Mail Handler Notifier plugin'
   author 'Roberto Piccini'
   description 'send a email to the user if he submitted invalid issue or invalid comment via email'
-  version '0.0.1'
+  version '2.0.0'
   url 'https://github.com/piccio/redmine_html_mail_handler_notifier'
   author_url 'https://github.com/piccio'
-  requires_redmine version: '2.6.0'
 
-  settings default: { enable_log: false }, partial: 'settings/html_mail_handler_notifier'
+  settings default: { 'enable_log' => false }, partial: 'settings/html_mail_handler_notifier'
 end
